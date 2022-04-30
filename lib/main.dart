57ee5423f547befa2860dd00eca30349cab1d518
OmkarWadekar <wadekaromkar08@gmail.com>
2022-04-30 16:45:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// import 'package:tip_calculator/Home.dart';
+
+import 'package:last_logic/Home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: BillSplitter(),
+  ));
+}
